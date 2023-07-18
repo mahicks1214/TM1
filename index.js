97eea3af59f82e292fad29db97b91ff27423a4bf
Michael Hickson <mhickson1214@gmail.com>
2023-07-18 10:02:14 -0700

    adding additional functions

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-console.log('Hello world!')
-
 function addTask(toDoList, input) {
-var list = document.getElementById("toDoList");
+var list = document.getElementById(toDoList);
 var listItem = document.createElement("li")
-listItem.innerText = inputField.value; // passed the field. 
+listItem.innerText = input.value; // passed the field. 
 list.appendChild(listItem);
+// May need to turn into a un-ordered list //
 return false; // stop submission
-}
 
+document.getElementById("unordered-list").appendChild(li);
+let button = document.createElement("button");
+button.innerText = "X";
+button.setAttribute = ("id", "b1");
+li.appendChild(button);
+button.addEventListener("click", () => li.parentNode.removeChild(li));
+document.getElementById("unordered-list").appendChild(li);
+
+li.appendChild(button);
+
+document.querySelectorAll('.buttonClass').forEach(item => {
+
+    item.addEventListener('click', event => {
+      change()
+})})}

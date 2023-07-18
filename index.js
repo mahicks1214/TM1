a894ba7be136129bfe02a5f3af2f56fc9863644e
FAngel2 <fernandoa2128@gmail.com>
2023-07-18 11:47:23 -0700

    changes v1

--- index.js
+++ index.js
@@ -3,21 +3,34 @@ var list = document.getElementById(toDoList);
 var listItem = document.createElement("li")
 listItem.innerText = input.value; // passed the field. 
 list.appendChild(listItem);
-// May need to turn into a un-ordered list //
-return false; // stop submission
-
 document.getElementById("unordered-list").appendChild(li);
+
 let button = document.createElement("button");
-button.innerText = "X";
-button.setAttribute = ("id", "b1");
-li.appendChild(button);
-button.addEventListener("click", () => li.parentNode.removeChild(li));
-document.getElementById("unordered-list").appendChild(li);
-
+button.innerText = "Done!";
+button.classList.add('buttonClass');
 li.appendChild(button);
 
-document.querySelectorAll('.buttonClass').forEach(item => {
+button.addEventListener("click", () => {
+  li.parentNode.removeChild(li);
+});
+document.getElementById("text-box").value = ""; // Clear the input field
 
-    item.addEventListener('click', event => {
-      change()
-})})}
+/*//add task text box
+document.getElementById("submit-btn").addEventListener("click", function() {
+  let text = document.getElementById("text-box").value;
+  let li = document.createElement("li");
+  li.id = "liId";
+  li.innerText = text;
+  document.getElementById("unordered-list").appendChild(li);
+
+  //X button deletes the task
+  let button = document.createElement("button");
+  button.innerText = "X";
+  button.classList.add('buttonClass');
+  li.appendChild(button);
+
+  button.addEventListener("click", () => {
+      li.parentNode.removeChild(li);
+  });
+*/
+

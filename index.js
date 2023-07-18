75e6ca53b1e94c017ffc5f12c76aa9e434fab223
Laura <lspulscher@gmail.com>
2023-07-18 09:57:04 -0600

    added submit button

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 console.log('Hello world!')
+
+function addTask(toDoList, input) {
+var list = document.getElementById("toDoList");
+var listItem = document.createElement("li")
+listItem.innerText = inputField.value; // passed the field. 
+list.appendChild(listItem);
+return false; // stop submission
+}
+
